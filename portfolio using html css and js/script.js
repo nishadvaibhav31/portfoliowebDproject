b34048b34997da60ca04d4df1bb98f4c2fc8a9c5
nishadvaibhav31 <135223842+nishadvaibhav31@users.noreply.github.com>
2024-10-15 01:22:17 +0530

    Delete portfolio using html css and js directory

--- portfolio using html css and js/script.js
+++
@@ -1,7 +0,0 @@
-const bar = document.querySelector('.bar');
-const navLinks = document.querySelector('.navbar');
-
-
-bar.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
